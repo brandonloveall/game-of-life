3a16ed15faa77ea72a1c9be748b413b851dd8c5c
lazikaz <souflakey@gmail.com>
2022-09-15 22:39:47 -0600

    changed range

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -5,9 +5,9 @@ function Settings(props){
     return(
         <div className="Settings">
             <label htmlFor="boxheight">Canvas height</label>
-            <input type="range" name="boxheight" min="5" max="20" onChange={(e) => {props.setHeight(e.target.value)}}/>
+            <input type="range" name="boxheight" min="3" max="20" onChange={(e) => {props.setHeight(e.target.value)}}/>
             <label htmlFor="boxwidth">Canvas width</label>
-            <input type="range" name="boxwidth" min="5" max="20" onChange={(e) => {props.setWidth(e.target.value)}}/>
+            <input type="range" name="boxwidth" min="3" max="20" onChange={(e) => {props.setWidth(e.target.value)}}/>
         </div>
     )
 }
